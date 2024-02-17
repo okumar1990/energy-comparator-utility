a4f52d16d782538c297eb9214906068a2beb4509
okumar1990 <93521438+okumar1990@users.noreply.github.com>
2024-02-17 10:20:13 +0530

    Initial commit

---
+++ src/main/java/uk/tw/energy/EnergyComparatorUtilityApplication.java
@@ -0,0 +1,13 @@
+package uk.tw.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyComparatorUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyComparatorUtilityApplication.class, args);
+	}
+
+}
