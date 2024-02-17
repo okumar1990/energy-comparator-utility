1c583cbe079e633f90b181d21a0435a92968b149
okumar1990 <93521438+okumar1990@users.noreply.github.com>
2024-02-17 17:06:28 +0530

    Initial commit

---
+++ src/test/java/uk/tw/energy/controller/ConsumptionCostControllerTest.java
@@ -0,0 +1,19 @@
+package uk.tw.energy.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ConsumptionCostControllerTest {
+
+	@BeforeEach
+	public void setUp() {
+
+	}
+
+	@Test
+	public void givenSmartMeterIdWithPricePlanAttached() {
+		
+	}
+	
+	
+}
