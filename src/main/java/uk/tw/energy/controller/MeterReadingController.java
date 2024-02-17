95315ac23974042755b9de7ae25b7c87f4a246ba
okumar1990 <93521438+okumar1990@users.noreply.github.com>
2024-02-17 18:06:47 +0530

    Initial commit

--- src/main/java/uk/tw/energy/controller/MeterReadingController.java
+++ src/main/java/uk/tw/energy/controller/MeterReadingController.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import uk.tw.energy.domain.ElectricityReading;
 import uk.tw.energy.domain.MeterReadings;
 import uk.tw.energy.service.MeterReadingService;
@@ -27,6 +31,11 @@ public class MeterReadingController {
 
     @PostMapping("/store")
     public ResponseEntity storeReadings(@RequestBody MeterReadings meterReadings) {
+    	try {
+    	System.out.println("meterReadings : "+new ObjectMapper().writeValueAsString(meterReadings));
+    	}catch(Exception e) {
+    		e.printStackTrace();
+    	}
         if (!isMeterReadingsValid(meterReadings)) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
